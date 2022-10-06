bcd516ebe7c56ec7e95cb982b063110269b25a02
AdityaBaxla <adityabaxla@gmail.com>
2022-10-06 21:17:11 +0530

    added login from selerium details

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ script to automaitcally login to netaccess and grant permission for 1 day
 choco install chromedriver
 
 chromedriver version has to match googlechrome you are using
+
+
+To do list:
+1.  login using selenium
+2.  make a task scheluar to automaitcally run the script 24hrs
+3.  put username and password import for safety
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from selenium import webdriver
 
+
 driver = webdriver.Chrome() #get chrome driver from installed driver
 
 driver.get('http://netaccess.iitm.ac.in')
 
-usernameBox = driver.find_element_by_xpath(//*[@id="username"])
-passBox = driver.find_element_by_xpath(//*[@id="password"])
+usernameBox = driver.find_element('xpath','//*[@id="username"]')
+passBox = driver.find_element('xpath','//*[@id="password"]')
+loginBtn = driver.find_element('xpath','//*[@id="submit"]')
+approveBtn = driver.find_element('xpath','//*[@id="content"]/div/div[1]/div[2]/a/span')
+oneDayBtn = driver.find_element('xpath','//*[@id="content"]/div/div[1]/form/div[2]/label')
+autorizeBtn = driver.find_element('xpath','//*[@id="approveBtn"]')
+
+usernameBox.send_keys('roll number')
+passBox.send_keys('password')
+
+#click login
+loginBtn.click()
+#wait for webpage to load
+#approveBtn.click()
+
+#wait for webpage to load
+oneDayBtn.click()
+autorizeBtn.click()
+
