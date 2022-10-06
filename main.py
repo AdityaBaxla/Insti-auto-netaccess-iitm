01fb7ea713dbbdc622b5e5d254ee0c5109231a8c
AdityaBaxla <adityabaxla@gmail.com>
2022-10-06 22:04:31 +0530

    login fully working

--- main.py
+++ main.py
@@ -3,24 +3,27 @@ from selenium import webdriver
 
 driver = webdriver.Chrome() #get chrome driver from installed driver
 
-driver.get('http://netaccess.iitm.ac.in')
+driver.get('http://netaccess.iitm.ac.in/account/login')
 
 usernameBox = driver.find_element('xpath','//*[@id="username"]')
 passBox = driver.find_element('xpath','//*[@id="password"]')
 loginBtn = driver.find_element('xpath','//*[@id="submit"]')
-approveBtn = driver.find_element('xpath','//*[@id="content"]/div/div[1]/div[2]/a/span')
-oneDayBtn = driver.find_element('xpath','//*[@id="content"]/div/div[1]/form/div[2]/label')
-autorizeBtn = driver.find_element('xpath','//*[@id="approveBtn"]')
 
-usernameBox.send_keys('roll number')
+
+usernameBox.send_keys('rollnumber')
 passBox.send_keys('password')
 
 #click login
 loginBtn.click()
-#wait for webpage to load
-#approveBtn.click()
 
 #wait for webpage to load
+approveBtn = driver.find_element('xpath','//*[@id="content"]/div/div[1]/div[2]/a/span')
+approveBtn.click()
+
+#wait for webpage to load
+oneDayBtn = driver.find_element('xpath','//*[@id="content"]/div/div[1]/form/div[2]/label')
+authorizeBtn = driver.find_element('xpath','//*[@id="approveBtn"]')
+
 oneDayBtn.click()
-autorizeBtn.click()
+authorizeBtn.click()
 
