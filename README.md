8ac98849884ed13e8b3ca5b896316867f22cdac8
AdityaBaxla <adityabaxla@gmail.com>
2022-10-06 20:51:06 +0530

    added fields for username and passsword

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Insti-auto-netaccess-iitm
 script to automaitcally login to netaccess and grant permission for 1 day
+
+choco install chromedriver
+
+chromedriver version has to match googlechrome you are using
--- main.py
+++ main.py
@@ -4,3 +4,5 @@ driver = webdriver.Chrome() #get chrome driver from installed driver
 
 driver.get('http://netaccess.iitm.ac.in')
 
+usernameBox = driver.find_element_by_xpath(//*[@id="username"])
+passBox = driver.find_element_by_xpath(//*[@id="password"])
